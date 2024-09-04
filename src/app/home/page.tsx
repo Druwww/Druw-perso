713c25e0772fca4090de5f91da3ed5059a052ca7
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2024-09-03 22:44:24 -0400

    add redux
    redu and test on it

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,34 @@
+'use client'
+import { Button } from "@mui/material";
 import NavBar from "../root/navbar";
+import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
+import { login, logout } from "@/lib/auth/authSlice";
 
 export default function Home() {
-  return (
-    <main>
-      <NavBar/>
-      <div>
-        Druw Perso website - Home Page !
-      </div>
-    </main>
-  );
+
+    const auth = useAppSelector(state => state.auth)
+    const dispatch = useAppDispatch()
+    return (
+        <main>
+        <NavBar/>
+        <div>
+            Druw Perso website - Home Page !
+        </div>
+        <div>
+            <Button onClick={() => dispatch(login("quentin@gmail.com"))}>
+                Login
+            </Button>
+        </div>
+        <div>
+            <Button onClick={() => dispatch(logout())}>
+                Logout
+            </Button>
+        </div>
+        <div>
+            <h3>
+                Email : {auth.email} - IsLogin : {auth.isLogin ? "True" : "False"}
+            </h3>
+        </div>
+        </main>
+    );
 }
