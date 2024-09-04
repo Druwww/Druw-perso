9d1bfe78cbcb93434d3ba7ead548235092a71876
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2024-09-04 17:46:20 -0400

    Sign In Page
    without auth yet

---
+++ src/app/signIn/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+import { useState } from 'react';
+import { Container, TextField, Button, Typography, Box } from '@mui/material';
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e : any) => {
+    e.preventDefault();
+    // Handle sign-up logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mt: 8,
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Sign In
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
